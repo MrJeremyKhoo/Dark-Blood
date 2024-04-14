20a996ae88649db944f60affb012b98a5cdb8d3d
MrJeremyKhoo <jeremykhoois@pm.me>
2024-04-15 01:12:50 +0800

    here

--- boom.sh
+++ boom.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Generate a random number between 0 and 1
-random_number=$(openssl rand -base64 32 | awk '{print $1 % 2}')
+# Generate a random sleep duration between 0 and 1800 seconds (30 minutes)
+sleep_duration=$(awk 'BEGIN{srand(); print int(rand()*1800)}')
 
-# Check if the random number is 0 (50% chance)
-if [[ $random_number == 0 ]]; then
+# Sleep for the random duration
+echo "Sleeping for $sleep_duration seconds"
+sleep $sleep_duration
+
+# Generate a random number between 0 and 1
+random_number=$(awk 'BEGIN{srand(); print int(rand()*2)}') 
+echo "$random_number"
+
+# Check if the random number is 1 (50% chance)
+if [[ $random_number == 1 ]]; then
     git --git-dir=$DIR/.git --work-tree=$DIR checkout $DIR/hash.txt
     git --git-dir=$DIR/.git --work-tree=$DIR pull
     rm $DIR/hash.txt
