91ba012121a657c962298090e107d9e9b5020a3a
MrJeremyKhoo <jeremykhoois@pm.me>
2024-04-22 19:30:12 +0800

    change randomeness

--- boom.sh
+++ boom.sh
@@ -2,14 +2,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Generate a random sleep duration between 0 and 1800 seconds (30 minutes)
-sleep_duration=$(awk 'BEGIN{srand(); print int(rand()*1800)}')
+sleep_duration=$(awk 'BEGIN{srand(); print int(rand()*3600)}')
 
 # Sleep for the random duration
 echo "Sleeping for $sleep_duration seconds"
 sleep $sleep_duration
 
 # Generate a random number between 0 and 1
-random_number=$(awk 'BEGIN{srand(); print int(rand()*2)}') 
+random_number=$(awk 'BEGIN{srand(); print int(rand()*5)}') 
 echo "$random_number"
 
 # Check if the random number is 1 (50% chance)
